e057356f3d14797c2689ed5f9aaffca5b1b4a8a8
yohannes <yohannestagel21@gmail.com>
2025-10-02 19:38:50 +0300

    i set up react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-600">home</h1>
+      <h1 className="bg-amber-300">the fers    hedech eko kkkk</h1>
+    </div>
+  )
+}
+
+export default App
